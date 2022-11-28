930407c3bc52959ccaf83fdc3b2aafa7eb95f990
simon-lowe <loewe.sim@gmail.com>
2022-11-28 21:12:53 +0100

    Basic AKM gender differential rent-sharing regression

--- AKM_Sim_Gender.R
+++ AKM_Sim_Gender.R
@@ -1,70 +1,46 @@
+library(data.table)
+library(reshape)
+library(lattice)
+library(gridExtra)
+library(mvtnorm)
+library(ggplot2)
+library(futile.logger)
+
 rm(list = ls())
 
-# Load packages -----------------------------------------------------------
-
-list.of.packages <- c("data.table", "tidyverse", "janitor", "lubridate",
-                      "reshape", "lattice", "gridExtra", "mvtnorm", "ggplot2", 
-                      "futile.logger", "gtools",
-                      "lfe", "fixest", "reshape2", "igraph", "ivreg",
-                      "microbenchmark"
-)
-
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
-
-invisible(lapply(list.of.packages, library, character.only = TRUE))
-rm(list.of.packages, new.packages)
-
-# Parameters --------------------------------------------------------------
-
 p <- list()
-# p$nk = 30  # firm types
-p$nk = 4  # firm types
-# p$nl = 10  # worker types
-p$nl = 2  # worker types
+p$nk = 30  # firm types
+p$nl = 10  # worker types
 
 p$alpha_sd = 1
 p$psi_sd   = 1
 
 # let's draw some FE
-p$psi   = with(p, qnorm(1:nk/(nk+1)) * psi_sd)
-p$alpha = with(p, qnorm(1:nl/(nl+1)) * alpha_sd)
+p$psi   = with(p,qnorm(1:nk/(nk+1)) * psi_sd)
+p$alpha = with(p,qnorm(1:nl/(nl+1)) * alpha_sd)
 
-# let's assume moving probability is fixed
+# let's assume moving PR is fixed
 p$lambda = 0.05
 
-p$csort = 0 # 0.5 # sorting effect
-p$cnetw = 0 # 0.2 # network effect
-p$csig  = 0.1 # unobserved variance of transition probability beyond FEs
-
-p$w_sigma = 0.8 # wage variance
-
-p$nt = 10 # Number of time periods
-p$ni = 1000 # Number of individuals
-# p$ni = 300000 # Number of individuals
-
-p$auto_rho <- 0.1
-
-set.seed(12345)
-
-# Creating the transition matrix ------------------------------------------
-
+p$csort = 0.5 # sorting effect
+p$cnetw = 0.2 # network effect
+p$csig  = 0.5 # 
 
 # lets create type specific transition matrices
 # we are going to use joint normal centered on different values
 # G[i,j,k] = Pr[worker i, at firm j, moves to firm k]
-
 getG <- function(p){
-  G = with(p, array(0, c(nl, nk, nk)))
-  for (l in 1:p$nl) for (j in 1:p$nk) {
+  G = with(p,array(0,c(nl,nk,nk)))
+  for (l in 1:p$nl) for (k in 1:p$nk) {
     # prob of moving is highest if dnorm(0)
-    G[l, j,] = with(p, dnorm(psi - cnetw*psi[j] - csort*alpha[l], sd = csig))
+    G[l,k,] = with(p,dnorm( psi - cnetw *psi[k] - csort * alpha[l],sd = csig ))
     # normalize to get transition matrix
-    G[l, j,] = G[l, j,]/sum(G[l, j,])
-  }
+    G[l,k,] = G[l,k,]/sum(G[l,k,])
+  } 
   return(G)
 }
 G <- getG(p)
+
 
 getH <- function(p,G){
   # we then solve for the stationary distribution over psis for each alpha value
@@ -77,89 +53,90 @@ getH <- function(p,G){
   }
   return(H)
 }
-H <- getH(p,G)
+H = getH(p,G)
 
 
-Plot1 = wireframe(G[1,,],aspect = c(1,1),xlab = "previous firm",ylab="next firm")
-Plot2 = wireframe(G[p$nl,,],aspect = c(1,1),xlab = "previous firm",ylab="next firm")
+Plot1=wireframe(G[1,,],aspect = c(1,1),xlab = "previous firm",ylab="next firm")
+Plot2=wireframe(G[p$nl,,],aspect = c(1,1),xlab = "previous firm",ylab="next firm")
 grid.arrange(Plot1, Plot2,nrow=1)
 
-wireframe(H,aspect = c(1,1), xlab = "worker", ylab = "firm")
 
-
-# Simulate the network ----------------------------------------------------
+p$nt = 6
+p$ni = 1000000
 
 sim <- function(p,G,H){
+  set.seed(1)
   
   # we simulate a panel
   network    = array(0,c(p$ni,p$nt))
   spellcount = array(0,c(p$ni,p$nt))
   A = rep(0,p$ni)
   
-  for(i in 1:p$ni) {
+  for (i in 1:p$ni) {
     # we draw the worker type
-    l = sample.int(p$nl, 1)
-    A[i] = l
+    l = sample.int(p$nl,1)
+    A[i]=l
     # at time 1, we draw from H
-    network[i,1] = sample.int(p$nk, 1, prob = H[l,]) # We draw firm type for individual i from given the stat dist for her type
+    network[i,1] = sample.int(p$nk,1,prob = H[l,])
     for (t in 2:p$nt) {
-      if (runif(1) < p$lambda) { # If there is a transition
-        network[i,t] = sample.int(p$nk, 1, prob = G[l, network[i,t-1],]) # transition P given by worker type and previous firm
-        spellcount[i,t] = spellcount[i,t-1] + 1
-      } else { # If no transition
+      if (runif(1)<p$lambda) {
+        network[i,t] = sample.int(p$nk,1,prob = G[l,network[i,t-1],])
+        spellcount[i,t] = spellcount[i,t-1] +1
+      } else {
         network[i,t]    = network[i,t-1]
         spellcount[i,t] = spellcount[i,t-1]
       }
     }
   }
   
-  data  = data.table(melt(network, c('i','t')))
-  data2 = data.table(melt(spellcount, c('i','t')))
+  data  = data.table(melt(network,c('i','t')))
+  data2 = data.table(melt(spellcount,c('i','t')))
   setnames(data,"value","k")
   data[,spell := data2$value]
-  data[,l := A[i],i]              # Assign for worker type to each individual
-  data[,alpha := p$alpha[l],l]    # Assign worker type FE
-  data[,psi := p$psi[k],k]        # Assign firm type FE
+  data[,l := A[i],i]
+  data[,alpha := p$alpha[l],l]
+  data[,psi := p$psi[k],k]
 }
 
 data <- sim(p,G,H)
 
-
-# Add gender --------------------------------------------------------------
-
-# tmp <- data.table(i = 1:p$ni, woman = rbinom(p$ni, 1, 0.48))
-# 
-# data <- merge(data, tmp, by = "i")
-# rm(tmp)
-
-# Assign identities to firms ----------------------------------------------
-
-addSpells <- function(p, dat){
+addSpells <- function(p,dat){
   firm_size = 10
   f_class_count = p$ni/(firm_size*p$nk*p$nt)
   
-  dspell <- dat[, list(len = .N), list(i, spell, k)]
-  dspell[, fid := sample(1:pmax(1, sum(len)/f_class_count), .N, replace = TRUE), k]
-  dspell[, fid := .GRP, list(k, fid)]
+  dspell <- dat[,list(len=.N),list(i,spell,k)]
+  dspell[,fid := sample( 1: pmax(1,sum(len)/f_class_count )   ,.N,replace=TRUE) , k]
+  dspell[,fid := .GRP, list(k,fid)]
   
-  setkey(dat, i, spell)
-  setkey(dspell, i, spell)
+  setkey(dat,i,spell)
+  setkey(dspell,i,spell)
   
-  dat[, fid:= dspell[dat, fid]]
+  dat[, fid:= dspell[dat,fid]]
 }
 
 addSpells(p,data)  # adds by reference to the same data.table object (no copy needed)
 
+data[, size := .N, by = .(t, fid)]
 
-# Simulating wages --------------------------------------------------------
 
 
+# Randomly woman
+# tmp <- data.table(i = 1:p$ni, woman = rbinom(p$ni, 1, 0.48))
+# data <- merge(data, tmp, by = "i")
+
+# Perfectly correlated with worker type
+data[, woman := l < p$nl/2, by = i]
+# data[, woman := l + rnorm(1, 0, 2) < p$nk/2, by = i]
+
+data[, h_wom := mean(woman), by = fid]
+summary(data$h_wom)
+
+p$w_sigma = 0.8
 addWage <- function(p,data){
+  # data[, lw := alpha + psi + p$w_sigma * rnorm(.N) ]
   data[, lw := alpha + 1*(1 - woman)*psi + 0.8*woman*psi + p$w_sigma * rnorm(.N)]
 }
-# addWage(p,data)
-
-# Restricting to the connected set ----------------------------------------
+addWage(p,data)
 
 concomp <- function(data){
   setkey(data,i,t)
@@ -195,63 +172,68 @@ concomp2 <- function(data){
   return(data)
 }
 
-extract_big_comp <- function(data){
-  tmp <- data[woman == TRUE, .(i, fid)] %>% unique()
-  
-  graph <- graph.data.frame(tmp, directed=FALSE)
-  comp <- components(graph)$membership
-  
-  data <- data %>%
-    left_join(
-      data.frame(
-        fid = as.numeric(names(comp)),
-        cs = comp
-      ), by = "fid"
-    )
-  setDT(data)
-  d1 <- data[, comp_size := .N, by = cs][comp_size == max(comp_size)][, cs := NULL]
-  data[, cs := NULL]
-  
-  tmp <- data[woman == FALSE, .(i, fid)] %>% unique()
-  
-  graph <- graph.data.frame(tmp, directed=FALSE)
-  comp <- components(graph)$membership
-  
-  data <- data %>%
-    left_join(
-      data.frame(
-        fid = as.numeric(names(comp)),
-        cs = comp
-      ), by = "fid"
-    )
-  setDT(data)
-  d2 <- data[, comp_size := .N, by = cs][comp_size == max(comp_size)][, cs := NULL]
-  return(rbind(d1, d2))
-}
+dat1 <- concomp2(data[woman == TRUE])
+dat2 <- concomp2(data[woman == FALSE])
 
-# Build the data ----------------------------------------------------------
+est1 <- feols(lw ~ 1 | i + fid, data = dat1)
 
-buildData <- function(p){
-  G <- getG(p)
-  H <- getH(p, G)
-  data <- sim(p, G, H)
-  tmp <- data.table(i = 1:p$ni, woman = rbinom(p$ni, 1, 0.48))
-  data <- merge(data, tmp, by = "i")
-  addSpells(p,data)
-  addWage(p,data)
-  return(data)
-}
-
-# Running the regression --------------------------------------------------
+fe1 <- fixef(est1)
 
 
-data <- buildData(p)
-data2 <- extract_big_comp(data)
+est2 <- feols(lw ~ 1 | i + fid, data = dat2)
 
-data2[, fid.wom := paste(fid, woman, sep = ".")]
+fe2 <- fixef(est2)
 
-est2 <- feols(lw ~ 1 | i + fid.wom, data = data2)
+f1 <- rownames_to_column(data.frame(fe1$fid), "fid") %>% mutate(fid = as.numeric(fid))
+f2 <- rownames_to_column(data.frame(fe2$fid), "fid") %>% mutate(fid = as.numeric(fid))
 
-fe <- fixef(est2)
-summary(fe)
+f <- merge(f1, f2, by = "fid")
 
+reg <- feols(data = f, fe1.fid ~ fe2.fid)
+etable(reg)
+
+reg <- feols(data = f, fe2.fid ~ fe1.fid)
+etable(reg)
+
+
+data[, split := rnorm(1) > 0, by = i]
+
+
+dat1 <- concomp2(data[woman == TRUE & split == TRUE])
+dat2 <- concomp2(data[woman == FALSE & split == TRUE])
+
+est1 <- feols(lw ~ 1 | i + fid, data = dat1)
+
+fe1 <- fixef(est1)
+
+
+est2 <- feols(lw ~ 1 | i + fid, data = dat2)
+
+fe2 <- fixef(est2)
+
+f1 <- rownames_to_column(data.frame(fe1$fid), "fid") %>% mutate(fid = as.numeric(fid))
+f2 <- rownames_to_column(data.frame(fe2$fid), "fid") %>% mutate(fid = as.numeric(fid))
+
+f <- merge(f1, f2)
+
+dat1 <- concomp2(data[woman == TRUE & split == FALSE])
+dat2 <- concomp2(data[woman == FALSE & split == FALSE])
+
+est1 <- feols(lw ~ 1 | i + fid, data = dat1)
+
+fe1 <- fixef(est1)
+
+
+est2 <- feols(lw ~ 1 | i + fid, data = dat2)
+
+fe2 <- fixef(est2)
+
+f1 <- rownames_to_column(data.frame(fe1$fid), "fid") %>% mutate(fid = as.numeric(fid))
+f2 <- rownames_to_column(data.frame(fe2$fid), "fid") %>% mutate(fid = as.numeric(fid))
+
+f2 <- merge(f1, f2)
+
+f <- merge(f, f2, by = "fid")
+
+reg <- feols(data = f, fe1.fid.x ~ 1 | fe2.fid.x ~ fe2.fid.y)
+etable(reg)
